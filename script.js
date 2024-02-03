b3479bae56201bbccb8976282e15afb4edea8332
miwanoff <marina.iva.noffa@gmail.com>
2024-02-03 12:57:49 +0200

    Add checkCollision

--- script.js
+++ script.js
@@ -92,6 +92,47 @@ Snake.prototype.draw = function () {
   }
 };
 
+Snake.prototype.move = function () {
+  let head = this.segments[0];
+  let newHead;
+  this.direction = this.nextDirection;
+  if (this.direction === "right") {
+    newHead = new Block(head.col + 1, head.row);
+  } else if (this.direction === "down") {
+    newHead = new Block(head.col, head.row + 1);
+  } else if (this.direction === "left") {
+    newHead = new Block(head.col - 1, head.row);
+  } else if (this.direction === "up") {
+    newHead = new Block(head.col, head.row - 1);
+  }
+
+  Snake.prototype.checkCollision = function (head) {
+    let leftCollision = head.col === 0;
+    let topCollision = head.row === 0;
+    let rightCollision = head.col === widthInBlocks - 1;
+    let bottomCollision = head.row === heightInBlocks - 1;
+    let wallCollision =
+      leftCollision || topCollision || rightCollision || bottomCollision;
+    let selfCollision = false;
+    for (let i = 0; i < this.segments.length; i++) {
+      if (head.equal(this.segments[i])) {
+        selfCollision = true;
+      }
+    }
+    return wallCollision || selfCollision;
+  };
+  if (this.checkCollision(newHead)) {
+    gameOver();
+    return;
+  }
+  this.segments.unshift(newHead);
+  //   if (newHead.equal(apple.position)) {
+  //     score++;
+  //     apple.move();
+  //   } else {
+  //     this.segments.pop();
+  //   }
+};
 // let sampleBlock = new Block(20, 20);
 // sampleBlock.drawSquare("blue");
 // let appleBlock = new Block(20, 20);
@@ -101,4 +142,6 @@ Snake.prototype.draw = function () {
 
 const snake = new Snake();
 
+snake.draw();
+snake.move();
 snake.draw();
